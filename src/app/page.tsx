259ef15175b477f777cba231e196cf08f0494019
thenanjali01 <anjali@softsaro.com>
2023-07-08 10:44:49 +0545

    added cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,42 @@
 import Stat from '@/components/ui/Stat';
 import { Heading, Paragraph } from '@edge-ui/react';
 import Image from 'next/image';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+// images
 import Search from '../../public/buySell.jpg';
 import CardOneImage from '../../public/keepKey.jpg';
 import CardTwoImage from '../../public/geo.jpg';
-
+// components
 import CustomCard from '../components/ui/Card';
+import Mode from '../components/ui/BuySellRent';
 
 export default function Home() {
+  const location = useRouter();
+
+  // const before_ = router.pathname;
+  // const location = before_.split('/').pop();
+
+  const [mode, setMode] = useState([
+    {
+      modeType: 'clickable',
+      modeName: 'Buy',
+      activeMode: 'buy'
+    },
+    {
+      modeType: 'clickable',
+      modeName: 'Sell',
+      activeMode: 'sell',
+      open: false
+    },
+    {
+      modeType: 'clickable',
+      modeName: 'Rent',
+      activeMode: 'rent',
+      open: false
+    }
+  ]);
   return (
     <main className="bg-scroll bg-my_bg_image h-[927px]">
       <div className="flex justify-between">
@@ -29,6 +58,49 @@ export default function Home() {
             <CustomCard title="Test" description="test" content="test" footer="test" src={CardTwoImage} />
           </div>
         </div> */}
+        <div>
+          {mode.map((modetype, index) => (
+            <React.Fragment key={index}>
+              {modetype.modeType === 'clickable' ? (
+                <Link
+                  href={`/${modetype.activeMode}`}
+                  scroll={true}
+                  className={
+                    location === `${modetype.activeMode}`
+                      ? 'flex justify-start my-2 bg-primary py-3 text-white rounded-lg items-center'
+                      : 'flex my-2 py-3 rounded-lg items-center'
+                  }
+                >
+                  <div className="">
+                    <span className="">{modetype.modeName}</span>
+                  </div>
+                </Link>
+              ) : (
+                <div
+                  className={!modetype.open ? 'flex flex-col my-2 py-3' : 'flex flex-col my-0 mt-2 py-0 pt-3'}
+                  key={index}
+                >
+                  <button
+                    onClick={() => {
+                      let newState = [...mode];
+                      newState[index].open = !newState[index].open;
+                      setMode(newState);
+                    }}
+                    className={
+                      !modetype.open === false
+                        ? 'flex w-full justify-start  rounded-lg items-center relative dropdown-navbar dropOpen'
+                        : 'flex w-full justify-start  rounded-lg items-center relative dropdown-navbar'
+                    }
+                  >
+                    <div className="">
+                      <span className="">{modetype.modeName}</span>
+                    </div>
+                  </button>
+                </div>
+              )}
+            </React.Fragment>
+          ))}
+        </div>
       </div>
     </main>
   );
